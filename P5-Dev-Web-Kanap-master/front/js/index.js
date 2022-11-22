500308c8a1484f1654f56a070b20b5ba1e6bf436
Assem <assem.youssef77@gmail.com>
2022-11-22 20:46:26 +0100

    second commit

--- P5-Dev-Web-Kanap-master/front/js/index.js
+++ P5-Dev-Web-Kanap-master/front/js/index.js
@@ -9,11 +9,11 @@ function affichageProduits(data) {
     let product = ""
 
     for (let i = 0; i < data.length; i++) {
-        product += `<a href="./product.html?id=${data[0]._id}">
+        product += `<a href="./product.html?id=${data[i]._id}">
     <article>
-        <img src="${data[0].imageUrl}" alt="${data[0].altTxt}">
-            <h3 class="productName">${data[0].name}</h3>
-            <p class="productDescription">${data[0].description}</p>
+        <img src="${data[i].imageUrl}" alt="${data[i].altTxt}">
+            <h3 class="productName">${data[i].name}</h3>
+            <p class="productDescription">${data[i].description}</p>
     </article>
     </a>
 `;
--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -5,13 +5,19 @@ const id = url.searchParams.get("id");
 fetch('http://localhost:3000/api/products')
 
 .then((reponse) => reponse.json())
-.then((data) => {
-    console.log(data);
+.then((data) => affichageProduits(data))
+
+function affichageProduits(data) {
+    console.log(data)
+
     let img = document.querySelector(".item__img");
+    img.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
     let title = document.getElementById("title");
+    title.innerHTML = data.name;
     let price = document.getElementById("price");
+    price.innerHTML = `${data.price}`;
     let description = document.getElementById("description");
+    description.innerHTML = data.description;
     let color = document.getElementById("colors");
-})
-
-
+    color.innerHTML = `<option value="${data.colors}">${data.colors}</option>`;
+    }
